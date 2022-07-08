a8720a62dfb3528bd3c8b01118b0c3cb3dc9a78d
ParkTaeYong <wkddksehd333.hello@gmail.com>
2022-07-08 18:11:56 +0900

    푸시

--- components/products/partials/newModule.vue
+++ components/products/partials/newModule.vue
@@ -1,5 +1,15 @@
 <template>
-  
+<div></div>
+    <!-- <swiper class="swiper" :modules="modules" navigation>
+    <swiper-slide class="slide">Slide 1</swiper-slide>
+    <swiper-slide class="slide">Slide 2</swiper-slide>
+    <swiper-slide class="slide">Slide 3</swiper-slide>
+    <swiper-slide class="slide">Slide 4</swiper-slide>
+    <swiper-slide class="slide">Slide 5</swiper-slide>
+    <swiper-slide class="slide">Slide 6</swiper-slide>
+    <swiper-slide class="slide">Slide 7</swiper-slide>
+    <swiper-slide class="slide">Slide 8</swiper-slide>
+  </swiper> -->
 </template>
 
 <script>
