f6f7947ac0c1d73ed5c4ded7e1de0b407f01b385
jeongheum <792wjdgma@naver.com>
2022-07-12 18:19:17 +0900

    adsfadsf

--- components/products/pages/Make.vue
+++ components/products/pages/Make.vue
@@ -2,15 +2,167 @@
   <section class="make-section" id="makeId">
     <!-- <intro-part :introSource="makeIntro"></intro-part> -->
     <span id="headingModuleTop" />
-    <heading-module
+    <!-- <heading-module
       :category="$t('productsText.makeText.introText.category')"
       :title="$t('productsText.makeText.introText.title')"
       :contents="$t('productsText.makeText.introText.contents')"
-    ></heading-module>
-
+    ></heading-module> -->
+    <!-- 
     <benefit-part
       :benefitArr="$t('productsText.makeText.benefitLists')"
-    ></benefit-part>
+    ></benefit-part> -->
+
+    <!-- 제품개요 -->
+    <div class="overViewMake">
+      <div class="overViewWrap">
+        <span class="overViewTit">
+          <p>제품개요</p>
+          <h2>쉽고 빠른 확장현실(XR) 콘텐츠 제작 솔루션</h2>
+          <span>
+            VIRNECT Make는 XR 콘텐츠로 제작 도구로, 비전문가도 마우스 클릭으로
+            XR 콘텐츠 제작이 가능합니다.
+            <p>
+              또한 문자, 사진, 비디오, 3D 모델은 물론 기존 작업 문서, 설비 도면
+              등을 그대로 제작에 빠르게 활용할 수 있습니다.
+            </p>
+          </span>
+        </span>
+        <span class="overViewCont">
+          <span>
+            <i
+              ><img
+                src="https://velog.velcdn.com/images/kyj0206/post/e7e696f2-8017-40bb-ad77-9243d372adc3/image.png"
+                alt="No Code and Simple"
+            /></i>
+            <p>No Code and Simple</p>
+            <p>코딩 없이, 클릭으로 쉽고 간단하게</p>
+            <p>XR 콘텐츠 제작이 가능합니다.</p>
+          </span>
+          <span>
+            <i
+              ><img
+                src="https://velog.velcdn.com/images/kyj0206/post/d8325cfd-f225-4696-9104-ca4f90385db8/image.png"
+                alt="Custom-made Content"
+            /></i>
+            <p>Custom-made Content</p>
+            <p>기존 리소스를 최대한 녹여낸</p>
+            <p>XR 콘텐츠 제작이 가능합니다.</p>
+          </span>
+          <span>
+            <i
+              ><img
+                src="https://velog.velcdn.com/images/kyj0206/post/97103073-33e1-46cb-b560-2919935cbb42/image.png"
+                alt="XR Device Accessibility"
+            /></i>
+            <p>XR Device Accessibility</p>
+            <p>XR 콘텐츠 접근성을 높이기 위한 다양한</p>
+            <p>디바이스별 콘텐츠 제작기능 제공</p>
+          </span>
+        </span>
+        <span class="overViewBox">
+          <ul>
+            <li>
+              <p>2,569</p>
+              <p>디운로드 수</p>
+            </li>
+            <li>
+              <p>38</p>
+              <p>Enterprise</p>
+            </li>
+            <li>
+              <p>51</p>
+              <p>계약건수</p>
+            </li>
+          </ul>
+        </span>
+      </div>
+    </div>
+
+    <!-- 기능소개 -->
+    <div class="featuresMake">
+      <div class="featuresWrap">
+        <span class="title">
+          <p>제품특징</p>
+        </span>
+        <div class="contents">
+          <span class="featuresTit">
+            <h2>
+              어려움 없이, XR 콘텐츠를 만드는
+              <p>노코드(No Code) 제작 솔루션!</p>
+            </h2>
+            <ul>
+              <li>
+                <i
+                  ><img
+                    src="https://velog.velcdn.com/images/kyj0206/post/6ee9bae6-3d0a-4c24-9868-c8a3af3759a9/image.png"
+                    alt="리스트1"
+                /></i>
+                <p>
+                  이미 보유된 이미지, 음성, 3D 가이드 등 여러가지 리소스를
+                  최대한 활용가능
+                </p>
+              </li>
+              <li>
+                <i
+                  ><img
+                    src="https://velog.velcdn.com/images/kyj0206/post/6ee9bae6-3d0a-4c24-9868-c8a3af3759a9/image.png"
+                    alt="리스트2"
+                /></i>
+                <p>
+                  초보 사용자를 위한 클릭, 드래그 & 드롭 등의 편리한 인터페이스
+                  제공
+                </p>
+              </li>
+              <li>
+                <i
+                  ><img
+                    src="https://velog.velcdn.com/images/kyj0206/post/6ee9bae6-3d0a-4c24-9868-c8a3af3759a9/image.png"
+                    alt="리스트3"
+                /></i>
+                <p>
+                  에셋 라이브러리로 리소스를 묶어서 사용할 수 있어 제작에 편리
+                </p>
+              </li>
+              <li>
+                <i
+                  ><img
+                    src="https://velog.velcdn.com/images/kyj0206/post/6ee9bae6-3d0a-4c24-9868-c8a3af3759a9/image.png"
+                    alt="리스트4"
+                /></i>
+                <p>
+                  모바일(안드로이드) 디바이스, 리얼웨어용, 홀로렌즈용 콘텐츠
+                  제작이 가능
+                </p>
+              </li>
+              <li>
+                <i
+                  ><img
+                    src="https://velog.velcdn.com/images/kyj0206/post/6ee9bae6-3d0a-4c24-9868-c8a3af3759a9/image.png"
+                    alt="리스트4"
+                /></i>
+                <p>여러 가지 타겟을 지정하여 제작 (QR, V, 플레인 타겟)</p>
+              </li>
+              <li>
+                <i
+                  ><img
+                    src="https://velog.velcdn.com/images/kyj0206/post/6ee9bae6-3d0a-4c24-9868-c8a3af3759a9/image.png"
+                    alt="리스트4"
+                /></i>
+                <p>IoT데이터 및 고객사 시스템과의 연동(구축형)</p>
+              </li>
+            </ul>
+          </span>
+          <span class="featuresCont">
+            <img
+              src="https://velog.velcdn.com/images/kyj0206/post/e0d52296-ef46-4e4d-9c6e-485ef6742765/image.png"
+              alt="features"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <!-- ㅇㄹㅁㅇㄴㄹ -->
     <span id="headingModuleBottom" />
     <heading-module
       :category="$t('productsText.makeText.funcTitle.category')"
@@ -185,4 +337,6 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "~assets/css/pages/products.scss";
+@import "~/assets/css/pages/overviewMake.scss";
+@import "~/assets/css/pages/FeaturesMake.scss";
 </style>
---
+++ components/products/partials/dataModule.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <!-- 데이타모듈 -->
+    <ul class="dataModule">
+      <li>
+        <span>
+          배터리 소모량
+          <p>(mAh/min)</p>
+        </span>
+        <span>
+          <p>35%</p>
+          <p>
+            <img
+              src="https://velog.velcdn.com/images/kyj0206/post/7997fb47-0a01-462e-889e-fa2b2baf5726/image.png"
+              alt="하락아이콘"
+            />
+          </p>
+        </span>
+      </li>
+      <li>
+        <span>
+          외부발열
+          <p>(℃)</p>
+        </span>
+        <span>
+          <p>22%</p>
+          <p>
+            <img
+              src="https://velog.velcdn.com/images/kyj0206/post/7997fb47-0a01-462e-889e-fa2b2baf5726/image.png"
+              alt="하락아이콘"
+            />
+          </p>
+        </span>
+      </li>
+      <li>
+        <span>
+          CPU점유율
+          <p>(%)</p>
+        </span>
+        <span>
+          <p>65%</p>
+          <p>
+            <img
+              src="https://velog.velcdn.com/images/kyj0206/post/7997fb47-0a01-462e-889e-fa2b2baf5726/image.png"
+              alt="하락아이콘"
+            />
+          </p>
+        </span>
+      </li>
+      <li>
+        <span>
+          메모리 점유율
+          <p>(KB)</p>
+        </span>
+        <span>
+          <p>58%</p>
+          <p>
+            <img
+              src="https://velog.velcdn.com/images/kyj0206/post/7997fb47-0a01-462e-889e-fa2b2baf5726/image.png"
+              alt="하락아이콘"
+            />
+          </p>
+        </span>
+      </li>
+    </ul>
+    <!-- 데이타모듈 -->
+  </section>
+</template>
+
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/css/common.scss";
+@import "~assets/css/reset.scss";
+@import "~/assets/css/modules/dataModule.scss";
+</style>
