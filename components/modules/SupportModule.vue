dad73274855b11f8f566cdab7edf3f681aee534b
jeongheum <792wjdgma@naver.com>
2022-07-17 22:00:36 +0900

    image커밋

--- components/modules/SupportModule.vue
+++ components/modules/SupportModule.vue
@@ -13,7 +13,7 @@
             <p>바로가기</p>
             <p>
               <img
-                src="https://velog.velcdn.com/images/kyj0206/post/d405b250-9807-41de-91f7-677076876929/image.png"
+                src="~/assets/images/pages/epc/arrow-heum.png"
                 alt="바로가기"
               />
             </p>
@@ -28,7 +28,7 @@
                 <p>Product Notice</p>
                 <p>
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/55729d34-c916-4922-97a2-ac8213bbeab6/image.png"
+                    src="~/assets/images/pages/epc/addbtn.png"
                     alt="Product Notice 더보기"
                   />
                 </p>
@@ -37,7 +37,7 @@
             <span>
               <a href="#">
                 <img
-                  src="https://velog.velcdn.com/images/kyj0206/post/0c8a11b8-b6bc-4fb9-b722-e4ae81438c34/image.png"
+                  src="~/assets/images/pages/epc/moduleimage123wd.png"
                   alt="Product Notice 이미지"
                 />
               </a>
@@ -49,7 +49,7 @@
                 <p>FAQ</p>
                 <p>
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/55729d34-c916-4922-97a2-ac8213bbeab6/image.png"
+                    src="~/assets/images/pages/epc/addbtn.png"
                     alt="FAQ 더보기"
                   />
                 </p>
@@ -59,7 +59,7 @@
               <span>
                 <nuxt-link to="/support/faq?category=1">
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/0fe02957-fa7f-42af-b714-cc9ba4e63cd9/image.png"
+                    src="~/assets/images/pages/epc/people124a.png"
                     alt="회원"
                   />
                 </nuxt-link>
@@ -67,7 +67,7 @@
               <span>
                 <nuxt-link to="/support/faq?category=3">
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/f966b265-fdce-4b0d-bc9e-74412568aa3c/image.png"
+                    src="~/assets/images/pages/epc/productImage1sd.png"
                     alt="제품"
                   />
                 </nuxt-link>
@@ -75,7 +75,7 @@
               <span>
                 <nuxt-link to="/support/faq?category=4">
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/0548505e-93ee-4c20-9182-2021dc74f965/image.png"
+                    src="~/assets/images/pages/epc/paygood12.png"
                     alt="결제"
                   />
                 </nuxt-link>
@@ -83,7 +83,7 @@
               <span>
                 <nuxt-link to="/support/faq?category=2">
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/f2a90d11-ad6b-485e-8d85-e237354ffdc9/image.png"
+                    src="~/assets/images/pages/epc/service12sad.png"
                     alt="서비스"
                   />
                 </nuxt-link>
@@ -91,7 +91,7 @@
               <span>
                 <nuxt-link to="/support/faq?category=6">
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/e3918984-3f62-4e19-811e-27d7d1a3b77c/image.png"
+                    src="~/assets/images/pages/epc/deviceaa1234.png"
                     alt="기기"
                   />
                 </nuxt-link>
@@ -99,7 +99,7 @@
               <span>
                 <nuxt-link to="/support/faq?category=5">
                   <img
-                    src="https://velog.velcdn.com/images/kyj0206/post/8198171b-41d4-4120-a382-e6f5ec307aaa/image.png"
+                    src="~/assets/images/pages/epc/etcaa1453sz.png"
                     alt="기타"
                   />
                 </nuxt-link>
@@ -113,7 +113,7 @@
               <p>학습센터</p>
               <p>
                 <img
-                  src="https://velog.velcdn.com/images/kyj0206/post/55729d34-c916-4922-97a2-ac8213bbeab6/image.png"
+                  src="~/assets/images/pages/epc/addbtn.png"
                   alt="학습센터 더보기"
                 />
               </p>
@@ -123,7 +123,7 @@
             <span>
               <nuxt-link to="/support/learning-center?category=1">
                 <img
-                  src="https://velog.velcdn.com/images/kyj0206/post/740a5bc9-f3ec-47b3-98c3-982ac2fb4533/image.png"
+                  src="~/assets/images/pages/epc/remoteicon11aav.png"
                   alt="Remote"
                 />
               </nuxt-link>
@@ -131,7 +131,7 @@
             <span>
               <nuxt-link to="/support/learning-center?category=2">
                 <img
-                  src="https://velog.velcdn.com/images/kyj0206/post/9aa3ce6b-0244-4832-add7-c037be467ca7/image.png"
+                  src="~/assets/images/pages/epc/makeIcon1aasdf.png"
                   alt="Make"
                 />
               </nuxt-link>
@@ -139,7 +139,7 @@
             <span>
               <nuxt-link to="/support/learning-center?category=3">
                 <img
-                  src="https://velog.velcdn.com/images/kyj0206/post/edb6a063-2cf3-4027-bf34-948e97aa0d77/image.png"
+                  src="~/assets/images/pages/epc/viewiconhihi.png"
                   alt="View"
                 />
               </nuxt-link>
@@ -156,7 +156,7 @@
           <div>
             <i>
               <img
-                src="https://velog.velcdn.com/images/kyj0206/post/c469bca8-6475-42c6-bb90-4d54edec3e7a/image.png"
+                src="~/assets/images/pages/epc/phoncall1azc.png"
                 alt="문의하기"
               />
             </i>
