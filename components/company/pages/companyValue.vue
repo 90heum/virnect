0110e6ef6b0a61ed304742d3eb227b06279444a8
jeongheum <792wjdgma@naver.com>
2022-07-18 16:28:25 +0900

    링크 수정

--- components/company/pages/companyValue.vue
+++ components/company/pages/companyValue.vue
@@ -73,7 +73,7 @@
           </span>
           <!-- 정유/화학/배터리 호버 -->
           <span class="oilHover">
-            <nuxt-link to="/value/oil_chemical_battery">
+            <nuxt-link to="/value/manufacture_construction">
               <span>
                 <p>제조/건설</p>
                 <i>
@@ -119,7 +119,7 @@
           </span>
           <!-- 공공/교육 호버 -->
           <span class="partsHover">
-            <nuxt-link to="/value/motor_parts">
+            <nuxt-link to="/value/public_education">
               <span>
                 <p>공공/교육</p>
                 <i>
@@ -165,7 +165,7 @@
           </span>
           <!-- 항공/철도 호버 -->
           <span class="airLineHover">
-            <nuxt-link to="/value/airline_railroad">
+            <nuxt-link to="/value/entertainment">
               <span>
                 <p>엔터테인먼트</p>
                 <i>
@@ -208,7 +208,7 @@
           </span>
           <!-- EPC 호버 -->
           <span class="epcHover">
-            <nuxt-link to="/value/epc">
+            <nuxt-link to="/value/etc">
               <span>
                 <p>기타</p>
                 <i>
