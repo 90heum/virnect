2700765a9ff4606ecf94316bdfba46e602cd04d0
jeongheum <792wjdgma@naver.com>
2022-07-21 20:02:47 +0900

    heum

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,16 +60,10 @@ export default {
         seo: true,
         //prefix_except_default <= 기존 no_prefix로바꾸고 호출문  setLocale(locale) 이거구현하니까 원하는모습나옴 대박
         strategy: "no_prefix",
-        defaultLocale: "ko",
+        defaultLocale: "en",
         lazy: true,
         langDir: "lang/",
-        detectBrowserLanguage: {
-          useCookie: true,
-          cookieDomain: null,
-          cookieKey: 'lang',
-          alwaysRedirect: true,
-          fallbackLocale: 'ko'
-        },
+
         locales: [
           {
             code: "ko",
