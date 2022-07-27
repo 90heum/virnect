1f04ea86aa2a570ad1886f386bf3c6b05fc8eadc
jeongheum <792wjdgma@naver.com>
2022-07-27 14:33:50 +0900

    설정

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,5 +99,11 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ["gsap-trial"]
-  }
+  },
+  axios: {
+    https: true,
+    proxy: true,
+
+  },
+
 }
